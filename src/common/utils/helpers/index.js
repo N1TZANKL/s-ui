29be77eebd2eda1610dee723b7834397d28a85b9
N1TZANKL <nitzan.kletter@gmail.com>
2020-08-13 11:27:48 +0300

    added helper functions

--- src/common/utils/helpers/index.js
+++ src/common/utils/helpers/index.js
@@ -1,1 +1,11 @@
-// import and export all helpers from here
+import * as GeneralHelpers from "./general";
+import * as DateHelpers from "./dates";
+import * as ObjectHelpers from "./objects";
+import * as StringHelpers from "./strings";
+
+export default {
+    ...GeneralHelpers,
+    ...DateHelpers,
+    ...ObjectHelpers,
+    ...StringHelpers
+};
